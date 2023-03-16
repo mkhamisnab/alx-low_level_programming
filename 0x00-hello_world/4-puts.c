9b6e4488989098cf4237994c81da0640584d0d07
mkhamisnab <mkhamisnabil@gmail.com>
2023-03-16 16:45:07 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,6 @@
  */
 int main(void)
 {
-	/**
-	 * return to executed 
-	  */
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
