814cd29e26254caf8205d334982fa89c8f067112
mkhamisnab <mkhamisnabil@gmail.com>
2023-03-21 15:59:40 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
-i * Return: 1 and prints + if n is greater than zero
+ * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
