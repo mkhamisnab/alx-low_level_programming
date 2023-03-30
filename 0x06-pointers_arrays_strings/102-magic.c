593c4fada8eefdf8ec7bb3f3db68b26af983a9de
mkhamisnab <mkhamisnabil@gmail.com>
2023-03-29 22:53:45 -0700

    Edited the magic file

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,7 +9,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
   /*
-   * write your line of code here...
+   * Main - magic function
    * Remember:
    * - you are not allowed to use a
    * - you are not allowed to modify p
